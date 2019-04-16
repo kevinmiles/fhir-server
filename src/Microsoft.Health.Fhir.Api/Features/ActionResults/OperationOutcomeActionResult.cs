bb9bdfb5d3c8d2009bb9e0f24fcc72a3fd1da677
Narasimhan Madabusi <namadabu@microsoft.com>
2019-04-16 16:22:55 -0700

    Added a new OperationOutcomeActionResult that will be returned whenever we throw an OperationOutcome exception (instead of using the existing FhirResult)

---
+++ src/Microsoft.Health.Fhir.Api/Features/ActionResults/OperationOutcomeActionResult.cs
@@ -0,0 +1,54 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+using EnsureThat;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.Health.Fhir.Api.Features.ActionResults
+{
+    /// <summary>
+    /// This action result is specifically used when we want to return an error
+    /// to the client with the appropriate OperationOutcome.
+    /// </summary>
+    public class OperationOutcomeActionResult : ActionResult
+    {
+        public OperationOutcomeActionResult(OperationOutcome outcome, HttpStatusCode statusCode)
+        {
+            EnsureArg.IsNotNull(outcome, nameof(outcome));
+
+            OperationOutcomeError = outcome;
+            StatusCode = statusCode;
+        }
+
+        public OperationOutcome OperationOutcomeError { get; }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public IHeaderDictionary Headers { get; } = new HeaderDictionary();
+
+        public override Task ExecuteResultAsync(ActionContext context)
+        {
+            EnsureArg.IsNotNull(context, nameof(context));
+
+            HttpResponse response = context.HttpContext.Response;
+            response.StatusCode = (int)StatusCode;
+
+            foreach (KeyValuePair<string, StringValues> header in Headers)
+            {
+                response.Headers.Add(header);
+            }
+
+            ActionResult result = new ObjectResult(OperationOutcomeError);
+
+            return result.ExecuteResultAsync(context);
+        }
+    }
+}
